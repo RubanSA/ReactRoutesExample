3d6e8d57fa9086db5b0386f6c4a61d24b7492e9b
Сергей Рубан <ruban@kr.step>
2020-12-10 19:41:57 +0200

    routes with parameters added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom"
+import { Main, About, News, NotFound, Product, Staff } from './Main';
+import NavMenu from './NavMenu';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
+      <Router>
+      <NavMenu></NavMenu>
+        <Switch>
+          <Route exact path="/" component={Main}></Route>
+          <Route path="/about" component={About}></Route>
+          <Route strict path="/news/" component={News}></Route>
+          <Route path="/staff/:department?/:id?" component={Staff}></Route>
+          <Route path="/products-:id(\d+)" component={Product}></Route>
+          <Route children={()=><h2>Not found from arrow function</h2>}></Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/NavMenu.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {Link, NavLink} from "react-router-dom";
+
+export default function NavMenu(){
+    return (
+        <>
+        <NavLink to="/" className="links">Main</NavLink>
+        <NavLink to="/news/" className="links">News</NavLink>
+        <NavLink to="/about" className="links">About</NavLink>
+        </>
+    )
+}
